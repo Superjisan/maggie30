1ecca0fcfca397acf4de1e5f0d63b5026aef4a80
Jisan Zaman <jisan.zaman@dynamo.works>
2020-05-12 10:23:30 -0400

    updated photos

--- src/photos.js
+++ src/photos.js
@@ -1,214 +1,53 @@
-import * as aliens from "./images/aliens.jpg"
-import * as artHat from "./images/arthat.jpg"
-import * as artWestco from "./images/artwestco.jpg";
-import * as artThesis from "./images/artthesis.jpg";
-import * as beccaTwin from "./images/beccatwin.jpg";
-import * as beyonce from "./images/beyonce.jpg";
-import * as crumping from "./images/crumping.jpg";
-import * as cuddlePuddle from "./images/cuddlepuddlewes1.jpg";
-import * as dance from "./images/dance.jpg";
-import * as dinosaur from "./images/dinosaur.jpg";
-import * as pretty from "./images/pretty.jpg";
-import * as faces from "./images/faces.jpg";
-import * as famChristmas from "./images/famchristmastree.jpg";
-import * as fierce from "./images/fierce.jpg";
-import * as firespinning from "./images/firespinning.jpg";
-import * as fwends from "./images/fwends.jpg";
-import * as gchat from "./images/gchat.jpg";
-import * as graduation from "./images/graduation.jpg"
-import * as guitar from "./images/guitar.jpg";
-import * as grotto from "./images/grotto.jpg";
-import * as headless from "./images/headless.jpg";
-import * as jisanKO from "./images/jisanpassout.jpg"; 
-import * as llama from "./images/llama.jpg";
-import * as missingNur from "./images/missingnur.jpg";
-import * as opera from "./images/opera.jpg";
-import * as orange from "./images/orange.jpg";
-import * as protest from "./images/protest.jpg";
-import * as rachel from "./images/realhappycouple.jpg"
-import * as starWarsCosplay from "./images/starwarsray.jpg";
-import * as usdan from "./images/usdan.jpg";
+import * as classyAf from "./images/classy-af.jpg"
+import * as hotness from "./images/hotness.jpg"
+import * as laurapose from "./images/laurapose.jpg"
+import * as notWeird from "./images/not-weird.jpg"
+import * as pigeonDance from "./images/pigeondance.jpg"
+import * as poisonIvy from "./images/poison-ivy.jpg"
+import * as scheming from "./images/scheming.jpg"
+
 
 export const photos = [
     {
-        src: aliens,
+        src: classyAf,
         width: 3,
         height: 4,
-        title: "You're just so wonderfully weird. I don't even know what this is a reference to, but I love it!"
+        title: "You are one classy lady Maggie! Love the umbrella"
     },
     {
-        src: artHat,
-        width: 3,
+        src: hotness,
+        width: 4,
         height: 4,
-        title: "A painting, a fantastic hat, and a Lexi shows you are always a great sport"
+        title: "Not sure what's really going on here, but I am going to interpret it as you find beauty whereever you go and that's majestic. Just like the muscular man here."
     },
     {
-        src: artWestco,
-        width: 4,
-        height: 3,
-        title: "You are just out of this talented. I can only imagine the countless hours that went into this. Never cease to be amazed by you."
-    },
-    {
-        src: artThesis,
-        width: 4,
-        height: 3,
-        title: "The amount of hard work you put into creating your own exhibition at Zilkha was unbelievable. You're incredibly hard working friend."
-    },
-    {
-        src: beccaTwin,
-        width: 3,
-        height: 4,
-        title: "Two of my fave people twinning on your trip. Fun fact about Becca: she was in my freshman hall and she was first person who ever said 'I worry about you Jisan', which was the sweetest thing I heard in my life at the time. So you pick em well."
-    },
-    {
-        src: beyonce,
-        width: 3,
-        height: 3,
-        title: "The time you prepped for Beyonce concert in H-town. It inspired me to see her in Philly. Always setting the trend you are friend."
-    },
-    {
-        src: crumping,
-        width: 4,
-        height: 3,
-        title: "Just your typical breaking it down. Thanks for introducing me to SMANG IT GIRL and so many other pop culture memes. I'd be a n00b without you."
-    },
-    {
-        src: cuddlePuddle ,
-        width: 3,
-        height: 3,
-        title: "This is one of many instances where you are so comfortable with giving away your amazing hugs and cuddles. Thanks for keeping your friends close, literally and figuratively."
-    },
-    {
-        src: dance,
+        src: laurapose,
         width: 5,
-        height: 3,
-        title: "I forgot how many TERPs you were in. This was an inspiring performance and you were always the best at all the dances from crumping, west african dance, modern and probably others I've never seen. Lucky to be friends with someone so talented."
-    },
-    {
-        src: dinosaur,
-        width: 3,
-        height: 3,
-        title: "A dinosaur, a copy machine, and a Lexi. The stories just write themselves. The amount of amusing things in your life is #lifegoals for me Lexi"
-    },
-    {
-        src: pretty,
-        width: 3,
-        height: 3,
-        title: "You're a beautiful person and this is just one shimmering example."
-    },
-    {
-        src: faces,
-        width: 4,
-        height: 3,
-        title: "Many people get up on my grill about my faces in pics, but a true friend joins me in making those faces. Thanks buddy."
-    },
-    {
-        src: famChristmas,
-        width: 4,
-        height: 3,
-        title: "I am so lucky to have met your family and I know how close you are with them and how much you love them and go out of your way to be there for them. You DA best"
-    },
-    {
-        src: fierce,
-        width: 3,
         height: 4,
-        title: "Everyone thinks you are nice but inside is this raging monster wanting to come out LOL JK. Another proof of why no one should ever mess with you."
+        title: "Master of poses and hikes you are. So grateful for the amount of nature adventures you dragged me to."
     },
     {
-        src: firespinning,
-        width: 4,
-        height: 3,
-        title: "That time you spun fire like it was NBD. People like me see that and think 'those people are crazy awesome'. You see firespinning and think 'Sounds Like Me! Let's get it!'"
-    },
-    {
-        src: fwends,
+        src: notWeird,
         width: 4,
         height: 4,
-        title: "The thing I love you is that you have so many various interests, so many groups, and so many friends that you can easily make your own. It speaks of how well rounded of a person you are. I don't know even know who these people are but I know they are lucky to have you in their lives."
+        title: "Never afraid to get weird, and that's what I love about you."
     },
     {
-        src: gchat,
+        src: pigeonDance,
+        width: 3,
+        height: 4,
+        title: "There's no one I would rather see do a pigeon dance than you as an offering to the gods."
+    },
+    {
+        src: poisonIvy,
+        width: 3,
+        height: 4,
+        title: "Your halloween costumes have always been on fire, even since Bjork in high school. Your creativity, artistry, and drive to have the most original costume is something I admire in you."
+    },
+    {
+        src: scheming,
         width: 5,
-        height: 3,
-        title: "That time we all had a little too much fun with face drawing on Google Hangouts. Thanks for always talking to me on Skype, Hangouts, phone, text, messenger, email, carrier pigeon. It is not something I take for granted and I am so glad you still respond after all these years."
-    },
-    {
-        src: graduation,
-        width: 4,
-        height: 3,
-        title: "This is not the best picture of you at our gradutation, but it is my website, so I can do whatever I want. Congrats Lexi we did it!"
-    },
-    {
-        src: guitar,
-        width: 4,
-        height: 3,
-        title: "Honestly, I didn't even know you played guitar but yet another constant new thing I find about you that reassures me how lucky I am to have you as a friend."
-    },
-    {
-        src: grotto,
-        width: 4,
-        height: 5,
-        title: "What happens in the grotto, stays in the grotto. One day, you'll have to explain this, but honestly you might be too cool for that kind of explanation. I miss James Gardner."
-    },
-    {
-        src: headless,
-        width: 4,
         height: 4,
-        title: "That time we hung out in the city and you were saddened by the headless statue. Thanks for spending that cold ass day with me TexLex."
+        title: "You are always scheming for the next adventure. Nothing but respect for that."
     },
-    {
-        src: jisanKO,
-        width: 4,
-        height: 4,
-        title: "That time you gave me a head massage and knocked me out. Use your powers wisely friend."
-    },
-    {
-        src: llama,
-        width: 3,
-        height: 3,
-        title: "This looks like you and the Llama are in a deep conversation. I would expect nothing less."
-    },
-    {
-        src: missingNur,
-        width: 5,
-        height: 3,
-        title: "This is a pic of you missing Nur. If there's any one picture that shows how much you care about your friends, it might be this. Thanks for always being 100% there, even when your friends aren't "
-    },
-    {
-        src: opera,
-        width: 4,
-        height: 3,
-        title: "That time you sang in an Opera without knowing how to read music. I mean DAYUM GIRL, you know no bounds!"
-    },
-    {
-        src: orange,
-        width: 3,
-        height: 3,
-        title: "There isn't a more fashionable person in my friend list than you."
-    },
-    {
-        src: protest,
-        width: 4,
-        height: 3,
-        title: "Thanks for being woke and fighting for a better world Lexi, it means a lot to me."
-    },
-    {
-        src: rachel,
-        width: 4,
-        height: 3,
-        title: "When I took this pic, Alain commented 'The REAL happy couple'. Which is so true. What a great pic of two lovely people I know."
-    },
-    {
-        src: starWarsCosplay,
-        width: 4,
-        height: 5,
-        title: "The force lives within you. Thanks for introducing me to Star Wars and watching a few with me during my RA nights. You are the last Jedi."
-    },
-    {
-        src: usdan,
-        width: 3,
-        height: 3,
-        title: "Still cute! We should get the cutest pic award in Facebook for this."
-    },
-
 ]
