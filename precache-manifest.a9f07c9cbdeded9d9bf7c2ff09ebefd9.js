8f8a03a2604a1aa737b8ea9721abe826307a778d
Jisan Zaman <jisan.zaman@dynamo.works>
2020-05-26 19:54:08 -0400

    Updates

---
+++ precache-manifest.a9f07c9cbdeded9d9bf7c2ff09ebefd9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796375445f21f38532c16d0c04427f0d",
+    "url": "/maggie30/index.html"
+  },
+  {
+    "revision": "43a21dd2144778f043a2",
+    "url": "/maggie30/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14f98a22b5744a589727",
+    "url": "/maggie30/static/js/2.0453bfc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maggie30/static/js/2.0453bfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a21dd2144778f043a2",
+    "url": "/maggie30/static/js/main.4ef7b476.chunk.js"
+  },
+  {
+    "revision": "d9fa32e27ac562e45773",
+    "url": "/maggie30/static/js/runtime-main.0c643d2f.js"
+  },
+  {
+    "revision": "3ec3c190e6265dd823fc1433aaa75cd9",
+    "url": "/maggie30/static/media/birthday-2016.3ec3c190.jpg"
+  },
+  {
+    "revision": "cff18c92674a0553fd54c4b5dbafd1a6",
+    "url": "/maggie30/static/media/bridesmaid.cff18c92.png"
+  },
+  {
+    "revision": "4154c51542a1b5d43ee45b56f2cd0df4",
+    "url": "/maggie30/static/media/camping.4154c515.jpg"
+  },
+  {
+    "revision": "a0ce9e13522b16717de7c3a1f601a611",
+    "url": "/maggie30/static/media/christmas-2017.a0ce9e13.jpg"
+  },
+  {
+    "revision": "0b247b5966855a0dcfabf16da93ac2ec",
+    "url": "/maggie30/static/media/classy-af.0b247b59.jpg"
+  },
+  {
+    "revision": "479a529c7593a87a17cabafe0a06f15f",
+    "url": "/maggie30/static/media/cuba-gangsta.479a529c.png"
+  },
+  {
+    "revision": "145a578cedf2b5da79777982f24cc3e5",
+    "url": "/maggie30/static/media/glasses.145a578c.png"
+  },
+  {
+    "revision": "fd7b2cd4432a2fd7ac771f5e210b2fda",
+    "url": "/maggie30/static/media/gold-toilet.fd7b2cd4.png"
+  },
+  {
+    "revision": "0fadb32aea0c36c3cbf458ce743233ab",
+    "url": "/maggie30/static/media/hotness.0fadb32a.jpg"
+  },
+  {
+    "revision": "57e943e9d05cd5009e069e2a7824227f",
+    "url": "/maggie30/static/media/japanese-dress.57e943e9.png"
+  },
+  {
+    "revision": "a944fa11afbb2aa526a03e33297d7978",
+    "url": "/maggie30/static/media/laurapose.a944fa11.jpg"
+  },
+  {
+    "revision": "c18bcfa3b372778774a2b18a8b3f70ae",
+    "url": "/maggie30/static/media/maddie.c18bcfa3.png"
+  },
+  {
+    "revision": "e70ab8b16e435b25c790eead26b4bd99",
+    "url": "/maggie30/static/media/noodles.e70ab8b1.png"
+  },
+  {
+    "revision": "f1c384e8a2c85d67887d145de215f0f4",
+    "url": "/maggie30/static/media/not-weird.f1c384e8.jpg"
+  },
+  {
+    "revision": "af9d5be2e8cfe5d92add94af2f2c6131",
+    "url": "/maggie30/static/media/peace.af9d5be2.jpg"
+  },
+  {
+    "revision": "c71bfec5ffe626ec98ffd44acb4081b9",
+    "url": "/maggie30/static/media/peppers.c71bfec5.png"
+  },
+  {
+    "revision": "cf2c8a8bf1d5f3efa94411e510629862",
+    "url": "/maggie30/static/media/pigeondance.cf2c8a8b.jpg"
+  },
+  {
+    "revision": "6e53249358d2071e47996532e1a93d65",
+    "url": "/maggie30/static/media/poison-ivy.6e532493.jpg"
+  },
+  {
+    "revision": "8fb82ec5a0ea8c20d6d3a78a4faa3221",
+    "url": "/maggie30/static/media/rock-elegant.8fb82ec5.jpg"
+  },
+  {
+    "revision": "f1e6150aeea5e6a5cf49d26e8a9d7c43",
+    "url": "/maggie30/static/media/scheming.f1e6150a.jpg"
+  },
+  {
+    "revision": "e8f7517f5d30112d5ed942b880493c31",
+    "url": "/maggie30/static/media/swimming.e8f7517f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maggie30/precache-manifest.3704946a476322e96e41001b426993c6.js"
+  "/maggie30/precache-manifest.a9f07c9cbdeded9d9bf7c2ff09ebefd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maggie30/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
