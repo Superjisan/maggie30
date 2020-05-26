fc741ff7d0cbfa2831fe7eafa95366d89712e53f
Jisan Zaman <jisan.zaman@dynamo.works>
2020-05-26 19:53:12 -0400

    add images

--- src/photos.js
+++ src/photos.js
@@ -1,53 +1,155 @@
-import * as classyAf from "./images/classy-af.jpg"
-import * as hotness from "./images/hotness.jpg"
-import * as laurapose from "./images/laurapose.jpg"
-import * as notWeird from "./images/not-weird.jpg"
-import * as pigeonDance from "./images/pigeondance.jpg"
-import * as poisonIvy from "./images/poison-ivy.jpg"
-import * as scheming from "./images/scheming.jpg"
-
+import * as birthday2016 from './images/birthday-2016.jpg';
+import * as bridesmaid from './images/bridesmaid.png';
+import * as camping from './images/camping.jpg';
+import * as christmas2017 from './images/christmas-2017.jpg';
+import * as classyAf from './images/classy-af.jpg';
+import * as cubaGangsta from './images/cuba-gangsta.png';
+import * as glasses from './images/glasses.png';
+import * as goldToilet from './images/gold-toilet.png';
+import * as hotness from './images/hotness.jpg';
+import * as japaneseDress from './images/japanese-dress.png';
+import * as laurapose from './images/laurapose.jpg';
+import * as maddie from './images/maddie.png';
+import * as maggiNoodles from './images/noodles.png';
+import * as notWeird from './images/not-weird.jpg';
+import * as peace from './images/peace.jpg';
+import * as peppers from './images/peppers.png';
+import * as pigeonDance from './images/pigeondance.jpg';
+import * as poisonIvy from './images/poison-ivy.jpg';
+import * as rockElegant from './images/rock-elegant.jpg';
+import * as scheming from './images/scheming.jpg';
+import * as swimming from './images/swimming.png';
 
 export const photos = [
     {
-        src: classyAf,
-        width: 3,
-        height: 4,
-        title: "You are one classy lady Maggie! Love the umbrella"
+        src: birthday2016,
+        width: 5,
+        height: 3,
+        title: 'I still remember that you came down all the way to DC for my 26th birthday and we baked cornbread. That was so sweet and it meant so much to me. Thank you friend. I love you!'
+    },
+	{
+		src: bridesmaid,
+		width: 4,
+		height: 4,
+		title:
+			'You clean up so nice. You looked stunning at the wedding and the speech was even better. We are lucky to have such a thoughtful friend.',
     },
     {
-        src: hotness,
-        width: 4,
-        height: 4,
-        title: "Not sure what's really going on here, but I am going to interpret it as you find beauty whereever you go and that's majestic. Just like the muscular man here."
-    },
-    {
-        src: laurapose,
+        src: camping,
         width: 5,
         height: 4,
-        title: "Master of poses and hikes you are. So grateful for the amount of nature adventures you dragged me to."
+        title: "Remember that one time we went camping, this was from there. What a shot! I am glad that you always encouraged the group to go and explore the big outdoors."
+    },
+	{
+		src: classyAf,
+		width: 3,
+		height: 4,
+		title: 'You are one classy lady Maggie! Love the umbrella',
     },
     {
-        src: notWeird,
+        src: christmas2017,
+        width: 5,
+        height: 3,
+        title: 'The amount of times you had us over at your house for delicious food is precious. Thank you for always welcoming me and others to your house. You and your fam are the best!'
+    },
+	{
+		src: cubaGangsta,
+		width: 4,
+		height: 4,
+		title:
+			'Only a true fashionista can pull this look off. You look positively bawler. I love that you can look and act badass while also being an amazingly supportive and sensitive friend.',
+	},
+	{
+		src: glasses,
+		width: 4,
+		height: 4,
+		title:
+			'You wear glasses? Who knew. Anyway, you look great here and I love that you can play with strangers, including dogs.',
+	},
+	{
+		src: goldToilet,
+		width: 4,
+		height: 4,
+		title: 'You rock even sitting on the toilet! Wish I had your skills Mags.',
+	},
+	{
+		src: hotness,
+		width: 4,
+		height: 4,
+		title:
+			"Not sure what's really going on here, but I am going to interpret it as you find beauty whereever you go and that's majestic. Just like the muscular man here. Love your eye for beautiful things friend.",
+    },
+    {
+        src: japaneseDress,
         width: 4,
         height: 4,
-        title: "Never afraid to get weird, and that's what I love about you."
+        title: "Lee sisters can surely rock that Kimono. I love that you're such an avid traveler. Can't wait to go on mad trips with ya after quarantine is over."
+    },
+	{
+		src: laurapose,
+		width: 5,
+		height: 4,
+		title: 'Master of poses and hikes you are. So grateful for the amount of nature adventures you dragged me to.',
     },
     {
-        src: pigeonDance,
-        width: 3,
+		src: maddie,
+		width: 4,
+		height: 4,
+		title: 'What a serene pic of you and Maddie. May we both have more peaceful days like these as our bones begin to crack as we are hitting 30.',
+    },
+    {
+        src: maggiNoodles,
+        width: 4,
         height: 4,
-        title: "There's no one I would rather see do a pigeon dance than you as an offering to the gods."
+        title: "Not everyone can pull off nostalgic food, and amazing costume at the same time. But you hit all the right flavor profiles. You rock Maggie!"
+    },
+	{
+		src: notWeird,
+		width: 5,
+		height: 3,
+		title: "Never afraid to get weird, and that's what I love about you.",
+	},
+	{
+		src: pigeonDance,
+		width: 3,
+		height: 4,
+		title: "There's no one I would rather see do a pigeon dance than you as an offering to the gods.",
     },
     {
-        src: poisonIvy,
-        width: 3,
+        src: peace,
+        width: 4,
         height: 4,
-        title: "Your halloween costumes have always been on fire, even since Bjork in high school. Your creativity, artistry, and drive to have the most original costume is something I admire in you."
+        title: "You maybe holding a peace sign. But inside you're saying 'DON'T MESS, I WILL CUT YOU!' jk, love your adorable azn faces friend."
     },
     {
-        src: scheming,
+        src: peppers,
+        width: 4,
+        height: 4,
+        title: "You grew hot peppers, how cool! (or hot!) love your commitment to long term goals and improvements. So proud of all of your accomplishments friend. Wish you nothing but all the best in the upcoming year."
+    },
+	{
+		src: poisonIvy,
+		width: 3,
+		height: 4,
+		title:
+			'Your halloween costumes have always been on fire, even since Bjork in high school. Your creativity, artistry, and drive to have the most original costume is something I admire in you.',
+    },
+    {
+        src: rockElegant,
         width: 5,
-        height: 4,
-        title: "You are always scheming for the next adventure. Nothing but respect for that."
+        height: 3,
+        title: 'A symbol of elegance. A burst of joy. A metaphor of grace. You are an amazing and beautiful soul friend.'
     },
-]
+	{
+		src: scheming,
+		width: 5,
+		height: 4,
+		title: 'You are always scheming for the next adventure. Nothing but respect for that.',
+    },
+    {
+        src: swimming,
+        width: 4,
+        height: 4,
+        title: 'Just a maggie and her thoughts in the water. You look so at home and relaxed. Hope this year allows you to have more of that fwend.'
+    }
+];
